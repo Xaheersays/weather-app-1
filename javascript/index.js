d1c52398105ab9d346d466b0ead25b8c8c84de8d
unknown <shzaheer514@gmail.com>
2022-11-03 22:59:17 +0530

    hourly-weather

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,6 @@
 const API_KEY = "ca644bb54b258eb289a8600b74525693";
 const getCurrentWeatherData = async()=>{
-    const city = "pune";
+    const city = "Mawsynram";
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
     return response.json()
 }
@@ -20,7 +20,7 @@ const  loadCurrrentForecast =({name,main:{temp,temp_max,temp_min},weather:[{desc
 }
 
 const formatTemperature = (temp)=>`${temp?.toFixed(1)}℃`;
-
+const createIconUrl =(icon)=>`http://openweathermap.org/img/wn/${icon}@2x.png`; 
 
 const getHourlyForecast = async ({name:city})=>{
     const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${"pune"}&appid=${API_KEY}&units=metric`);
@@ -35,12 +35,25 @@ const getHourlyForecast = async ({name:city})=>{
 
 const loadHourlyForecast = (hourlyForecast) =>{
     console.log(hourlyForecast);
+    let dataFor12Hours = hourlyForecast.slice(1,13);
+    console.log(dataFor12Hours);
+    const hourlyContainer = document.querySelector(".hourly-container");
+    let innerHtmlString =``;
+
+    for (let {temp,icon,dt_txt} of dataFor12Hours){
+        innerHtmlString += `<article>
+        <h2 class="time">${dt_txt.split(" ")[1]}</h2>
+        <img class="icon" src="${createIconUrl(icon)}" />
+        <p class="hourly-temp">${formatTemperature(temp)}</p>
+    </article>`
+    }
+    hourlyContainer.innerHTML= innerHtmlString;
 }
 
 
 document.addEventListener("DOMContentLoaded",async ()=>{
     const currentWeather =await getCurrentWeatherData();
-    console.log(currentWeather)
     loadCurrrentForecast(currentWeather);
     const hourlyForecast = await getHourlyForecast(currentWeather); 
+    loadHourlyForecast(hourlyForecast);
 });
